fe68e23181d7597ae5a8ef3eb011b72b49a151ea
shinsang97 <103565931+shinsang97@users.noreply.github.com>
2023-10-31 12:13:52 +0900

    first commit

---
+++ src/main/java/com/shinsang/shinsangbackend/ShinsangBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shinsang.shinsangbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinsangBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShinsangBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shinsang/shinsangbackend/ShinsangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinsang.shinsangbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinsangBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
